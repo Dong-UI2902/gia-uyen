0cd243285c46312219796c4a461e3b8d238ac2f3
Dong-UI2902 <dongnv2902@gmail.com>
2025-08-01 15:02:23 +0700

    update url marketing

--- src/pages/Marketing.tsx
+++ src/pages/Marketing.tsx
@@ -19,7 +19,7 @@ const Item = [
     name: "SOCIAL MEDIA MARKETING",
     tag: "STEVEN PASSARO",
     img: "/assets/images/project/marketing/6.png",
-    url: "https://drive.google.com/file/d/1JprA6JehpI3DpJDqy4UX8I8lSBPZRlqa/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/1D1wfZsgBeoCSlgLcH6isK04Aet6hRBYO/view?usp=drive_link",
   },
   {
     name: "INTERNATIONAL BUSINESS",
